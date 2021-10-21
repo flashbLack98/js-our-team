54e0bc64088deee88c7814b65d3f1a0704cc9159
flashbLack98 <npalakuttige98@icloud.com>
2021-10-21 15:57:02 +0200

    create one object

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const containerCard = document.querySelector(".team-card");
+
+const card = `<div class="card-image">
+<img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett" />
+</div >
+    <div class="card-text">
+        <h3>Wayne Barnett</h3>
+        <p>Founder & CEO</p>
+    </div>`;
+
+containerCard.innerHTML += card;
+
+
+const memberList = [
+    {
+        name: "Wayne Barnett",
+        role: "Angela Caroll",
+        image: "Walter Gordon"
+    }
+];
+
+
